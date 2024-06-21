de9495fc267b3363945bc381d0c424c631e18f64
Bogdan Nitica <bogdan@riadig.com>
2024-06-21 12:53:03 +0300

    remove scripts

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y software-properties-common build-essential
+sudo apt-get install -y build-essential
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install -y curl ansible g++ 
+sudo apt-get install -y curl ansible 
 sudo apt-get autoremove -y
 sudo apt-get clean autoclean -y
 ansible-playbook -v local.yml
